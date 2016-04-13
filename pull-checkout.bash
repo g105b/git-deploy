6d959ef42c4fe9b038915cad13789a3e6e58735b
Greg Bowler <greg.bowler@g105b.com>
2016-04-13 15:47:56 +0100

    Auto deploy repo

--- pull-checkout.bash
+++ pull-checkout.bash
@@ -1,8 +1,25 @@
 #!/bin/bash
+set -e
 dir="$(dirname "$0")"
 source "$dir/config.ini"
 
-cd $repo_url
+if [ -d "$dir/config.d" ]; then
+	if [ -f "$dir/config.d/$1.ini" ]; then
+		source "$dir/config.d/$1.ini"
+	fi
+fi
+
+if [ ! -f $repo_dir ]; then
+	git clone -b $webhook_branch --single-branch $repo_url $repo_dir
+fi
+
+cd $repo_dir
 git pull $repo_url
 git --work-tree=$destination_path --git-dir=$repo_dir.git checkout -f
-./post-checkout.bash
+"$dir/post-checkout.bash $1"
+
+if [ -d "$dir/post-checkout.d" ]; then
+	if [ -f "$dir/post-checkout.d/$1.bash" ]; then
+		"$dir/post-checkout.d/$1.bash"
+	fi
+fi
