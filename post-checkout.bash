f83a0c0147844f38095fd183610b135cb9316330
Greg Bowler <greg.bowler@g105b.com>
2016-06-06 11:44:43 +0100

    Export variables

--- post-checkout.bash
+++ post-checkout.bash
@@ -4,4 +4,14 @@
 # slashes replaced with underscores
 set -e
 dir="$(dirname "$0")"
+
+export webhook_event=$webhook_event
+export webhook_branch=$webhook_branch
+export received_branch=$received_branch
+export webhook_log_path=$webhook_log_path
+export repo_url=$repo_url
+export repo_dir=$repo_dir
+export destination_path=$destination_path
+export ssh_private_key=$ssh_private_key
+
 eval "$dir/db.php $1"
