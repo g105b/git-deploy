79ec17aaee15c23b2f46c7a0aef97f5db1f45689
Greg Bowler <greg.bowler@g105b.com>
2016-06-06 11:47:56 +0100

    Do not re-set environment variables

--- db.php
+++ db.php
@@ -1,20 +1,6 @@
 #!/usr/bin/php
 <?php
-foreach(parse_ini_file(__DIR__ . "/config.ini") as $key => $value) {
-	putenv("$key=$value");
-}
-
 $repoNameNoSlashes = $argv[1];
-
-if(is_dir(__DIR__ . "/config.d")) {
-	$iniFile = __DIR__ . "/config.d/$repoNameNoSlashes.ini";
-
-	if(file_exists($iniFile)) {
-		foreach(parse_ini_file($iniFile) as $key => $value) {
-			putenv("$key=$value");
-		}
-	}
-}
 
 $dbMigrationPath = getenv("db_migration_path");
 if(!empty($dbMigrationPath)) {
